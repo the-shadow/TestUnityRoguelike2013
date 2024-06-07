06a1243c6153389c5dd889a603a18fe5a108892d
RJ <ryanlewjackson@gmail.com>
2024-06-07 01:05:07 -0700

    Add files via upload
    Player Character's sword attack

---
+++ Sword_Attack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword_Attack : MonoBehaviour
+{
+    private int m_nSwordPower;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_nSwordPower = 1;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            coll.gameObject.SendMessage("Sword_hit", m_nSwordPower);
+        }
+        else if (coll.gameObject.tag == "Food_Jar")
+        {
+            coll.gameObject.SendMessage("Food_Jar_hit");
+        }
+        else if (coll.gameObject.tag == "Wooden_Crate")
+        {
+            coll.gameObject.SendMessage("Wooden_Crate_hit");
+        }
+    }
+}
