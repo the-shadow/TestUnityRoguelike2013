73f2d5d1b6ec3addc9e3ac708c19aa4540bc6f44
RJ <ryanlewjackson@gmail.com>
2024-06-07 00:58:07 -0700

    Add files via upload
    Any game actor (ie player, enemy, helper npc) that will use the nav node system

---
+++ Pathfinding_NavnodeAgent.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Pathfinding_NavNodeAgentType
+{
+    NONE = 0,
+    Player,
+    Shade,
+    Slime,
+
+}
+;
+public class Pathfinding_NavnodeAgent : MonoBehaviour {
+
+    public Pathfinding_NavNodeAgentType m_PathfindAgent_Type;
+
+    public Transform m_Target;  //where we currently want to go
+
+    public Transform m_TargetGoal; //Location of wherever we are trying to reach
+
+    public int m_nGoalPreviousRoomID; //Room that the Goal was in last time we performed Pathfind Check
+
+    public Queue<NavNode> m_Path;
+
+   //
+
+    public IndoorDungeonGenerator m_IndoorDungeonGenerator;
+
+
+    public int m_nCurrentRoomID;
+    // Use this for initialization
+    void Start () {
+        m_PathfindAgent_Type = Pathfinding_NavNodeAgentType.NONE;        
+    }
+
+    public void SetType(Pathfinding_NavNodeAgentType agentType)
+    {
+        m_PathfindAgent_Type = agentType;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
